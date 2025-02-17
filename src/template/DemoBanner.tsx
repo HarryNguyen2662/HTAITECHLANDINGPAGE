a09d3e22f84fab145c79a802e3fbedd733bdd8d7
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2025-02-17 13:41:40 -0500

    add botton banner

--- src/template/DemoBanner.tsx
+++ src/template/DemoBanner.tsx
@@ -4,8 +4,8 @@ import { StickyBanner } from '@/features/landing/StickyBanner';
 
 export const DemoBanner = () => (
   <StickyBanner>
-    Live Demo of SaaS Boilerplate -
+    Explore our provided services
     {' '}
-    <Link href="/sign-up">Explore the User Dashboard</Link>
+    <Link href="/services">Explore our provided services</Link>
   </StickyBanner>
 );
